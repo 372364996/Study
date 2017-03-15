616b986203b598b266dcaa368157a41df46b51d0
杜鹏 <dupeng.tommy@outlook.com>
2017-03-15 21:20:40 +0800

    第二篇Blog练习完毕

---
+++ Study.MVC/Study.MVC/Study.MVC/Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Study.MVC.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string Welcome( string name ,int  num)
+        {
+            return string.Format("{0}第{1}访问固定的Action",name,num);
+        }
+    }
+}
