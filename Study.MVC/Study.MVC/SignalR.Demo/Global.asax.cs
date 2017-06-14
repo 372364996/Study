e25b42ef245b9391e93319619b6b75ed9bb46e33
杜鹏 <dupeng.tommy@outlook.com>
2017-06-15 00:00:22 +0800

    +增加signalr

---
+++ Study.MVC/Study.MVC/SignalR.Demo/Global.asax.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+namespace SignalR.Demo
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            RouteTable.Routes.MapHubs();
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+        }
+    }
+}
